935494df4f38eb233b93001df68366dbc77932fd
unknown <ravibs1994@gmail.com>
2020-11-03 16:01:19 +0530

    UC1 Create Empty board for 10 positions

--- TickTacToeGame.java
+++ TickTacToeGame.java
@@ -1,8 +1,12 @@
 public class TickTacToeGame{
+    char array[]=new char[10];
+     public void createBoard(){
+       System.out.println("Welcome to Tictactoe game");
+	for(int i=1;i<=9;i++){
+	    array[i]=' ';
+	}
 
-    public void createBoard(){
-       System.out.println("Welcome to tictactoe game");
-    }
+     }
     public static void main (String args[]){
         TickTacToeGame t1=new TickTacToeGame();
         t1.createBoard();
