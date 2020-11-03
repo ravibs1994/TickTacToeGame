dc179ec51b687998461415a350ad7577af4d56ba
unknown <ravibs1994@gmail.com>
2020-11-03 15:53:44 +0530

    Master Commit Welcome to TickTacToeGame

---
+++ TickTacToeGame.java
@@ -0,0 +1,10 @@
+public class TickTacToeGame{
+
+    public void createBoard(){
+       System.out.println("Welcome to tictactoe game");
+    }
+    public static void main (String args[]){
+        TickTacToeGame t1=new TickTacToeGame();
+        t1.createBoard();
+    }
+}
