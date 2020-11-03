9a2d41741f76be5263085497fe79dcdd2377e901
unknown <ravibs1994@gmail.com>
2020-11-03 17:50:23 +0530

    UC2:Allow User to Enter Character

--- TickTacToeGame.java
+++ TickTacToeGame.java
@@ -1,5 +1,11 @@
 public class TickTacToeGame{
     char array[]=new char[10];
+    char word = ' ';
+	char requiredX = 'X';
+	char requiredO = 'O';
+	char comValueO = 'O';
+	char comValueX = 'X';
+
      public void createBoard(){
        System.out.println("Welcome to Tictactoe game");
 	for(int i=1;i<=9;i++){
@@ -7,8 +13,24 @@ public class TickTacToeGame{
 	}
 
      }
+     public void userInputX() {
+       System.out.println("Enter X or O character ");
+       word = sc.next().charAt(0);
+       if (word == requiredX) {
+           comValueO='O';
+	} else {
+	   System.out.println("Enter Valid character");
+	}
+	if (word == requiredO) {
+	 } else {
+	     System.out.println("Enter Valid character");
+	
+	  }
+       }
     public static void main (String args[]){
         TickTacToeGame t1=new TickTacToeGame();
         t1.createBoard();
+	t1.userInputX();
     }
 }
+	
